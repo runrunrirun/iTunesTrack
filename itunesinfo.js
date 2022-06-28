6ac129ac94f88f4911481732a002311764c05809
slyborg <1388365+runrunrirun@users.noreply.github.com>
2022-06-27 23:29:50 -0500

    Changed to run function implmentation to eliminate the extra library load .

--- itunesinfo.js
+++ itunesinfo.js
@@ -4,58 +4,56 @@
 //
 // fsb 06/2022
 
-// Need this to get exit()
-ObjC.import("stdlib");
+function run(argv) {
+  if (Application("Music").running()) {
+    var app = Application.currentApplication();
+    app.includeStandardAdditions = true;
 
-if (Application("Music").running()) {
-  var app = Application.currentApplication();
-  app.includeStandardAdditions = true;
+    var thistrack = Application("Music").currentTrack;
+    var streamTitle = Application("Music").currentStreamTitle();
+    var streamURL = Application("Music").currentStreamURL();
+    var tr_title = thistrack.name();
+    var tr_artist = thistrack.artist();
+    var tr_album = thistrack.album();
+    var tr_type = thistrack.class(); // fileTrack urlTrack audioCDTrack
+    var tr_year = thistrack.year();
+    var trackinfo = "🎶 Now Playing: ";
 
-  var thistrack = Application("Music").currentTrack;
-  var streamTitle = Application("Music").currentStreamTitle();
-  var streamURL = Application("Music").currentStreamURL();
-  var tr_title = thistrack.name();
-  var tr_artist = thistrack.artist();
-  var tr_album = thistrack.album();
-  var tr_type = thistrack.class(); // fileTrack urlTrack audioCDTrack
-  var tr_year = thistrack.year();
-  var trackinfo = "🎶 Now Playing: ";
+    try {
+      if (!app) throw "Couldn't get current application!";
+      if (!thistrack) throw "No track data!";
+    } catch (err) {
+      console.log(err);
+      return -1; d
+    }
 
-  try {
-    if (!app) throw "Couldn't get current application!"
-    if (!thistrack) throw "No track data!";
-  } catch (err) {
-    console.log(err);
-    $.exit(-1);
-  }
+    // Create trackinfo string
+    if (tr_type == "urlTrack" && streamTitle) {
+      // If we find a streamTitle, it's an old-style Shoutcast stream
+      // assumes that track or artist names don't contain dash
+      tr_title = streamTitle.substring(streamTitle.indexOf("-") + 2);
+      tr_artist = streamTitle.substring(0, streamTitle.indexOf("-"));
+      trackinfo += `"${tr_title}"` + " By: " + tr_artist + "Via: " + streamURL;
+    } else {
+      // Everything else looks the same, but we'll only add info if present in iTunes metadata
+      trackinfo += `"${tr_title}"`;
+      if (tr_artist) {
+        trackinfo += " By: " + tr_artist;
+      }
+      if (tr_album) {
+        trackinfo += " From: " + tr_album;
+      }
+      if (tr_year) {
+        trackinfo += ` [${tr_year}]`;
+      }
+      trackinfo += "🎶 ";
+    }
+    // Output track info
+    console.log(trackinfo);
 
-  // Create trackinfo string
-  if (tr_type == "urlTrack" && streamTitle) {
-    // If we find a streamTitle, it's an old-style Shoutcast stream
-    // assumes that track or artist names don't contain dash
-    tr_title = streamTitle.substring(streamTitle.indexOf("-") + 2);
-    tr_artist = streamTitle.substring(0, streamTitle.indexOf("-"));
-    trackinfo += `"${tr_title}"` + " By: " + tr_artist + "Via: " + streamURL;
+    // Copy trackinfo to clipboard
+    app.setTheClipboardTo(trackinfo);
   } else {
-    // Everything else looks the same, but we'll only add info if present in iTunes metadata
-    trackinfo += `"${tr_title}"`;
-    if (tr_artist) {
-      trackinfo += " By: " + tr_artist;
-    }
-    if (tr_album) {
-      trackinfo += " From: " + tr_album;
-    }
-    if (tr_year) {
-      trackinfo += ` [${tr_year}]`;
-    }
-    trackinfo += "🎶 ";
+    console.log("Nothing playing!");
   }
-  // Output track info
-  console.log(trackinfo);
-
-  // Copy trackinfo to clipboard
-  app.setTheClipboardTo(trackinfo);
-
-} else {
-  console.log("Nothing playing!");
 }
